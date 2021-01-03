6947f07abe22a6580849280aae13ff97888b94b0
ghaith alaydi <ghaith.alaydi@gmail.com>
2021-01-03 13:55:00 +0200

    Initialization

---
+++ apps.js
@@ -0,0 +1,42 @@
+'use strict';
+var seattle = {
+    minCustomers: 23,
+    maxCustomers: 65,
+    averCookiesCustomer: 6.3,
+    numberOfCustomers = function (){
+Math.random();}
+};
+
+var tokyo = {
+    minCustomers: 3,
+    maxCustomers: 24,
+    averCookiesCustomer: 1.2,
+    numberOfCustomers = function (){
+Math.random();}
+};
+
+var dubai= {
+    minCustomers: 11,
+    maxCustomers: 28,
+    averCookiesCustomer: 3.7,
+    numberOfCustomers = function (){
+Math.random();}
+};
+
+var paris = {
+    minCustomers: 20,
+    maxCustomers: 38,
+    averCookiesCustomer: 2.3,
+    numberOfCustomers = function (){
+Math.random();}
+};
+
+var lima = {
+    minCustomers: 2,
+    maxCustomers: 16,
+    averCookiesCustomer: 4.6,
+    numberOfCustomers = function (){
+Math.random();}
+};
+var workingHours = ['6am:','7am:','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','total'];
+
